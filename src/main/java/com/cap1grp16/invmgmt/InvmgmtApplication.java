ad7a01c732320cfbb3ab5c92f2beceaa4f6255ee
YAGOO (not real) <93402922+rosnuker@users.noreply.github.com>
2024-04-05 11:54:45 +0800

    Update InvmgmtApplication.java
    - Added a logger to inform us if the backend server is running.

--- src/main/java/com/cap1grp16/invmgmt/InvmgmtApplication.java
+++ src/main/java/com/cap1grp16/invmgmt/InvmgmtApplication.java
@@ -1,13 +1,19 @@
 package com.cap1grp16.invmgmt;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InvmgmtApplication {
 
+	static Logger logger = Logger.getLogger(InvmgmtApplication.class.getName());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(InvmgmtApplication.class, args);
+
+		logger.info("Backend is up and running!");
 	}
 
 }
